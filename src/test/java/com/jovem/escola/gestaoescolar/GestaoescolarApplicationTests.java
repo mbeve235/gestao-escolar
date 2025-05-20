2e876189c135b362183fdd5b970c4b85a5ba60ab
mbeve234 <mbeve235@email.com>
2025-05-20 13:19:18 +0200

    primeiro commit

---
+++ src/test/java/com/jovem/escola/gestaoescolar/GestaoescolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovem.escola.gestaoescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoescolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
